42dc4afa3985a7bb41b2fde02b91f35e4b96bc45
Yashpal01997 <Yashpal01997@gmail.com>
2026-06-28 09:11:16 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dsa-tracker/sw.js', { scope: '/dsa-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/DSA-Tracker/sw.js', { scope: '/DSA-Tracker/' })})}
